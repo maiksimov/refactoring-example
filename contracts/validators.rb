7fb45460e0a836aec622102d71e225cb52474e35
Michael <somov.michail.yur@gmail.com>
2020-02-01 21:16:57 +0200

    Fix validator returns nil

--- contracts/validators.rb
+++ contracts/validators.rb
@@ -4,28 +4,24 @@ module Contracts
       return true unless balance.negative?
 
       puts I18n.t('no_money')
-      false
     end
 
     def amount_valid?(amount)
       return true if amount.positive?
 
       puts I18n.t('wrong_money_amount')
-      false
     end
 
     def card_index_valid?(selected_card_index, context)
       return true if selected_card_index.between?(1, context.current_account.card.length)
 
       puts I18n.t('choose_correct_card')
-      false
     end
 
     def account_have_cards?(cards)
       return true if cards.any?
 
       puts I18n.t('no_active_cards')
-      false
     end
   end
 end
